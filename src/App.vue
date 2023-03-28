2d0cd1a4ac7a8c4c80b6fba6a76e5c2f8f8437e0
unknown <190401162@student.umri.ac.id>
2023-03-28 14:25:24 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1>saya anime</h1>
+  <img src="./assets/luffy.jpg">
+</template>
+
+
